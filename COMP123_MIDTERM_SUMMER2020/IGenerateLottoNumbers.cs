a2b0a98bbed133cba6025d41d95aec8f07c24975
Param Vir Singh Sandhu <psandh63@my.centennialcollege.ca>
2020-07-17 09:09:40 -0400

    Add project files.

---
+++ COMP123_MIDTERM_SUMMER2020/IGenerateLottoNumbers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_MIDTERM_SUMMER2020
+{
+    // Defines GenerateLotttoNumbers Method
+    interface IGenerateLottoNumbers
+    {
+        void GenerateLottoNumbers();
+    }
+}
---
+++ COMP123_MIDTERM_SUMMER2020/LottoMax.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_MIDTERM_SUMMER2020
+{
+    /**
+   * <summary>
+   * This class is a subclass of the LottoGame abstract superclass
+   * </summary>
+   * 
+   * @class LottoMax
+   */
+    public class LottoMax : LottoGame, IGenerateLottoNumbers
+    {
+        /**
+         * <summary>
+         * This constructor does not take any parameters but satisfies the
+         * base constructor requirements by send the elementNumber and setSize
+         * </summary>
+         * 
+         * @constructor
+         */
+        public LottoMax()
+            : base(7, 49)
+        {
+
+        }
+
+        // CREATE the public GenerateLottoNumbers method here ----------------
+        public void GenerateLottoNumbers()
+        {
+            for (int i = 1; i <= ElementNumber; i++)
+            {
+                PickElements();
+
+                Console.Write($"Ticket { i } : ");
+                Console.WriteLine(ToString());
+
+            }
+        }
+    }
+}
